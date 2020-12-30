9eecc8450b4058e7a1e3e1bdcccfc94d3d516ee1
Ben De Leon <bendeleonjr@hotmail.com>
2020-12-30 13:06:42 +0800

    updated print

--- src/com/steven/programs/BasicPrint.java
+++ src/com/steven/programs/BasicPrint.java
@@ -4,5 +4,6 @@ public class BasicPrint {
     public static void main(String[] args){
         System.out.println("Hello Steve!");
         System.out.println("Another print!");
+        System.out.println("Ignore");
     }
 }
