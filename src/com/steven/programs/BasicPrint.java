fbfbb61d3918ebcd7430b42cf2fcd902de758769
Ben De Leon <bendeleonjr@hotmail.com>
2020-12-30 12:14:58 +0800

    updated print

--- src/com/steven/programs/BasicPrint.java
+++ src/com/steven/programs/BasicPrint.java
@@ -3,5 +3,6 @@ package com.steven.programs;
 public class BasicPrint {
     public static void main(String[] args){
         System.out.println("Hello Steve!");
+        System.out.println("Another print!");
     }
 }
