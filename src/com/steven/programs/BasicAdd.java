e1a9f688be84de5d14aebe712807c7f0c247d965
Ben De Leon <bendeleonjr@hotmail.com>
2020-12-30 12:08:17 +0800

    Initial commit

---
+++ src/com/steven/programs/BasicAdd.java
@@ -0,0 +1,11 @@
+package com.steven.programs;
+
+public class BasicAdd {
+    public static void main(String[] args){
+        int x = 100 + 2;
+        System.out.println(x);
+
+        int y = x + 3;
+        System.out.println(y);
+    }
+}
